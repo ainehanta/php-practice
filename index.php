67acf6a12532d2c93258d976079c054d4e98e4a5
Makoto Uju <ainehanta@hotmail.co.jp>
2017-01-16 09:32:10 +0900

    インデックスでファイル一覧

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<html>
+<head>
+  <meta charset="utf8" >
+  <title>習作</title>
+</head>
+<body>
+<h1>List of Contents</h1>
+
 <?php
-phpinfo();
+foreach(glob('*.php') as $file){
+  if(is_file($file)){
+    $file = htmlspecialchars($file);
+    echo "<div><a href=\"$file\">$file</a></div>";
+  }
+}
 ?>
+
+</body>
+</html>
