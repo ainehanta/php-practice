905708d8ca0297cb846189432ba285c3578e270a
Makoto Uju <ainehanta@hotmail.co.jp>
2017-01-16 10:01:57 +0900

    Access Counter

---
+++ access-counter.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+  <meta charset="utf8" >
+  <title>アクセスカウンタ</title>
+</head>
+<body>
+<h1>アクセスカウンタ</h1>
+
+<?php
+  if(!file_exists('count.txt')) {
+    file_put_contents('count.txt', '0');
+  }
+
+  $f = fopen("count.txt", "r+");
+  $c = fgets($f, 10);
+  $c = $c + 1;
+  fseek($f, 0);
+  fputs($f, $c);
+  fclose($f);
+?>
+
+<p>あなたは <?php echo $c; ?> 人目のお客様です。</p>
+
+</body>
+</html>
